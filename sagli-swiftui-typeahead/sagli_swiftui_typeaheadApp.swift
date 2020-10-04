139d7dbc9f5a9845f3a51aa6129b50144c46075f
Ola Marius Hoff Sagli <ola@sagliconsulting.com>
2020-10-04 10:54:27 +0200

    Initial Commit

---
+++ sagli-swiftui-typeahead/sagli_swiftui_typeaheadApp.swift
@@ -0,0 +1,17 @@
+//
+//  sagli_swiftui_typeaheadApp.swift
+//  sagli-swiftui-typeahead
+//
+//  Created by Ola Marius Sagli on 04/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct sagli_swiftui_typeaheadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
